27493033574b8baa8ccd09d4490d5bb2227354fd
cibere <71997063+cibere@users.noreply.github.com>
2025-01-15 17:06:09 -0800

    force reinstall pywin32 if there was a problem with the install

--- main.py
+++ main.py
@@ -14,6 +14,42 @@ sys.path.append(
 sys.path.append(os.path.join(parent_folder_path, "lib", "win32", "lib"))
 sys.path.append(os.path.join(parent_folder_path, "lib", "win32"))
 
+from flogin.utils import print, setup_logging
+
+setup_logging()
+
+# Detect 'ImportError: DLL load failed while importing win32event: The specified module could not be found. '
+# and automatically reinstall if found
+
+try:
+    import pywintypes
+except ImportError:
+    import subprocess
+
+    libs = (
+        os.path.join("venv", "lib", "site-packages")
+        if os.path.exists("venv")
+        else "lib"
+    )
+
+    subprocess.run(
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "--force-reinstall",
+            "-U",
+            "pywin32",
+            "-t",
+            libs,
+        ],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    print(f"Installed pywin32 at {libs!r}")
+
+
 from plugin.plugin import ScreenBrightnessPlugin
 
 if __name__ == "__main__":
