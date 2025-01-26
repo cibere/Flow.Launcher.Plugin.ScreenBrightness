01d3a6b335a5edcf7a8781d0cc142b906478f3ed
cibere <71997063+cibere@users.noreply.github.com>
2025-01-26 08:29:21 -0800

    use flogin.Pip to ensure pywin32 is installed

--- main.py
+++ main.py
@@ -1,54 +1,26 @@
-import os
 import sys
+from pathlib import Path
 
-parent_folder_path = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(parent_folder_path)
-sys.path.append(os.path.join(parent_folder_path, "lib"))
-sys.path.append(os.path.join(parent_folder_path, "venv", "lib", "site-packages"))
-sys.path.append(
-    os.path.join(parent_folder_path, "venv", "lib", "site-packages", "win32", "lib")
+root = Path(__file__).parent
+sys.path.extend(
+    [
+        path.as_posix()
+        for path in (
+            root,
+            root / "lib",
+            root / "lib" / "win32",
+            root / "lib" / "win32" / "lib",
+        )
+    ]
 )
-sys.path.append(
-    os.path.join(parent_folder_path, "venv", "lib", "site-packages", "win32")
-)
-sys.path.append(os.path.join(parent_folder_path, "lib", "win32", "lib"))
-sys.path.append(os.path.join(parent_folder_path, "lib", "win32"))
 
-from flogin.utils import print, setup_logging
+from flogin.utils import setup_logging
+from flogin import Pip
 
 setup_logging()
 
-# Detect 'ImportError: DLL load failed while importing win32event: The specified module could not be found. '
-# and automatically reinstall if found
-
-try:
-    import pywintypes
-except ImportError:
-    import subprocess
-
-    libs = (
-        os.path.join("venv", "lib", "site-packages")
-        if os.path.exists("venv")
-        else "lib"
-    )
-
-    subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "pip",
-            "install",
-            "--force-reinstall",
-            "-U",
-            "pywin32",
-            "-t",
-            libs,
-        ],
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL,
-    )
-    print(f"Installed pywin32 at {libs!r}")
-
+with Pip() as pip:
+    pip.ensure_installed("pywin32", module="pywintypes")
 
 from plugin.plugin import ScreenBrightnessPlugin
 
